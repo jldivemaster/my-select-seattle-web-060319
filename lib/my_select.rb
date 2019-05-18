8852e17804b51d446ca3d50b30c4c3cf829c0e65
Jason Leach <jlspursfan@gmail.com>
2019-05-18 17:35:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,12 @@ def my_select(collection)
   if collection.length == 0
     puts "This block should not run!"
   else
+    new_array = []
     i = 0
     while i < collection.length
-      yield(collection[i])
+      new_array << yield(collection[i])
       i += 1
     end
+    return new_array
   end
 end
